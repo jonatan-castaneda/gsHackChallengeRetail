75020c232d125a7d13d18fe2bea468e1e1ce50df
Ian Castañeda <castanedachaverojonatan@gmail.com>
2019-08-25 09:04:08 -0500

    GSChallenge fix error

--- python/ston/gsRetail/services.py
+++ python/ston/gsRetail/services.py
@@ -29,9 +29,12 @@ def get_products(search, page=None, output_type="full",
 def getCategories(level,category_one,category_two):
     text_search = request(conf.api_url+'catalog_system/pub/category/tree/3/',params=entities,method='GET')
     if level == '1':
-        categories = [{'id':text_search['id'], 'name':text_search['name']} for text_search]
+        categories = [{'id':cat['id'], 'name':cat['name']} for cat in text_search]
     if level == '2':
         for cat_one in text_search:
             if cat_one['id'] == category_one:
                 categories = [{'id':cat_two['id'], 'name':cat_two['name']} for cat_two in cat_one['children']]
                 break
+    return {
+        "categories": categories
+    }
